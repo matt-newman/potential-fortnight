5fbb0e6d06dffb81158907c8c2890f55aae152e1
Matt Newman <matthew.newman@cloudcall.com>
2024-08-30 00:58:47 +0100

    some minor cleanup of unused vars

--- src/components/calendar/day.tsx
+++ src/components/calendar/day.tsx
@@ -15,16 +15,14 @@ export type CalendarDay = {
 
 export function Day(props: DayProps):ReactElement {
     const { day, index } = props;
-    const { date: strDate, streak_type: type } = day;
-    const date = new Date(strDate);
+    const { streak_type: type } = day;
 
-    const dayNum = date.getDate();
-    const isValidDay = dayNum > -1 || type === 'empty';
-    const isEmpty = type === 'empty';
     const fallbackType = type || 'empty';
+    const isEmpty = type === 'empty';
     const today = new Date();
     const month = getMonthInfo(today.getMonth(), today.getFullYear());
     const isToday = (month.shiftFromMondayToFirstDayOfMonth + today.getDate() - 1) === index; // getDate() - 1, to shift date to be 0 indexed like array
+    const todayClass = isToday ? 'day--today' : '';
     let weekClass = index % 7 === 0 ? ' week-start ' : ''; // start of week
     weekClass += index % 7 === 6 ? ' week-end ' : ''; // end of week
 
@@ -32,12 +30,10 @@ export function Day(props: DayProps):ReactElement {
         <>
             {/* div vs span tweak to allow css :first-of-type selector */}
             {isEmpty && (
-                <div className={`day day--${fallbackType} ${isToday ? 'day--today' : ''} ${weekClass}`}></div>
-            )}
-            {isValidDay && !isEmpty && (
-                <span className={`day day--${fallbackType} day--streak ${isToday ? 'day--today' : ''} ${weekClass}`}>
-                    {/* {today.getDate()} - {todayOffset} - {index} */}
-                    {isValidDay && type !== 'empty' && (<img className={'day-image'} alt={`${type}`} src={`/images/${type}.png`} />)}
+                <div className={`day day--${fallbackType} ${todayClass} ${weekClass}`}></div>
+            ) || (
+                <span className={`day day--streak day--${fallbackType} ${todayClass} ${weekClass}`}>
+                    {type !== 'empty' && (<img className={'day-image'} alt={`${type}`} src={`/images/${type}.png`} />)}
                 </span>
             )}
         </>
