28904e59304eae6b18e006847ca643d51e6d25b3
Matt Newman <matthew.newman@cloudcall.com>
2024-08-29 13:13:40 +0100

    added copy of notes, and mocked data shape

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,5 +1,38 @@
 import './calendar.css';
 
+/*
+The data structure you receive: You receive the following data in an object:
+
+streak_length = the number of days streak they’re on
+calendar = a list of Day objects, one for each day in a month
+Each Day object has fields:
+streak_type: the type of streak they got that day, can be None. Different streaks have different images
+date
+images = a list of all the streak images
+
+{
+    streak_length: 1,
+    calendar: [
+        {
+            streak_type: '',
+            date: '',
+        }
+    ]
+    images: [],
+}
+*/
+
+const mockDataShape = {
+    streak_length: 1,
+    calendar: [
+        {
+            streak_type: '',
+            date: '',
+        }
+    ],
+    images: [],
+}
+
 export function Calendar() {
   return (
     <div className="Calendar">
